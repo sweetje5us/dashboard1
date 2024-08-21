fe56e6fbd545d35278fe0f425f1cf9dc1058c0c5
Sweet Jesus <bryloveeugene@gmail.com>
2024-08-21 22:58:13 +0500

    full first block

--- src/partials/dashboard/DashboardCard01.jsx
+++ src/partials/dashboard/DashboardCard01.jsx
@@ -1,89 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import LineChart from '../../charts/LineChart01';
 import { chartAreaGradient } from '../../charts/ChartjsConfig';
 import EditMenu from '../../components/DropdownEditMenu';
 
-// Import utilities
-import { tailwindConfig, hexToRGB } from '../../utils/Utils';
 
-function DashboardCard01() {
+function getRise(a) {
+  const name1 = a;
+  const [userData, setUserData] = useState([]);
+  // fetching api data
+  useEffect(() => {
+    fetch('https://api.sunrisesunset.io/json?lat=56.271461&lng=57.999168&date=today')
+      .then(res => res.json())
+      .then(data => setUserData(data.results))
+    
+  }, []
+)
+  return (userData[a])
+}
 
-  const chartData = {
-    labels: [
-      '12-01-2022',
-      '01-01-2023',
-      '02-01-2023',
-      '03-01-2023',
-      '04-01-2023',
-      '05-01-2023',
-      '06-01-2023',
-      '07-01-2023',
-      '08-01-2023',
-      '09-01-2023',
-      '10-01-2023',
-      '11-01-2023',
-      '12-01-2023',
-      '01-01-2024',
-      '02-01-2024',
-      '03-01-2024',
-      '04-01-2024',
-      '05-01-2024',
-      '06-01-2024',
-      '07-01-2024',
-      '08-01-2024',
-      '09-01-2024',
-      '10-01-2024',
-      '11-01-2024',
-      '12-01-2024',
-      '01-01-2025',
-    ],
-    datasets: [
-      // Indigo line
-      {
-        data: [732, 610, 610, 504, 504, 504, 349, 349, 504, 342, 504, 610, 391, 192, 154, 273, 191, 191, 126, 263, 349, 252, 423, 622, 470, 532],
-        fill: true,
-        backgroundColor: function(context) {
-          const chart = context.chart;
-          const {ctx, chartArea} = chart;
-          return chartAreaGradient(ctx, chartArea, [
-            { stop: 0, color: `rgba(${hexToRGB(tailwindConfig().theme.colors.violet[500])}, 0)` },
-            { stop: 1, color: `rgba(${hexToRGB(tailwindConfig().theme.colors.violet[500])}, 0.2)` }
-          ]);
-        },            
-        borderColor: tailwindConfig().theme.colors.violet[500],
-        borderWidth: 2,
-        pointRadius: 0,
-        pointHoverRadius: 3,
-        pointBackgroundColor: tailwindConfig().theme.colors.violet[500],
-        pointHoverBackgroundColor: tailwindConfig().theme.colors.violet[500],
-        pointBorderWidth: 0,
-        pointHoverBorderWidth: 0,
-        clip: 20,
-        tension: 0.2,
-      },
-      // Gray line
-      {
-        data: [532, 532, 532, 404, 404, 314, 314, 314, 314, 314, 234, 314, 234, 234, 314, 314, 314, 388, 314, 202, 202, 202, 202, 314, 720, 642],
-        borderColor: `rgba(${hexToRGB(tailwindConfig().theme.colors.gray[500])}, 0.25)`,
-        borderWidth: 2,
-        pointRadius: 0,
-        pointHoverRadius: 3,
-        pointBackgroundColor: `rgba(${hexToRGB(tailwindConfig().theme.colors.gray[500])}, 0.25)`,
-        pointHoverBackgroundColor: `rgba(${hexToRGB(tailwindConfig().theme.colors.gray[500])}, 0.25)`,
-        pointBorderWidth: 0,
-        pointHoverBorderWidth: 0,
-        clip: 20,
-        tension: 0.2,
-      },
-    ],
-  };
 
-  return (
-    <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-gray-800 shadow-sm rounded-xl">
+
+export const DashboardCard01 = () => { 
+  const [time, setTime] = useState(new Date()); 
+  
+
+ 
+  useEffect(() => { 
+    const interval = setInterval(() => { 
+      setTime(new Date()); 
+    }, 1000); 
+    if (minutes<10){
+
+    }
+    return () => clearInterval(interval); 
+  }, []); 
+ 
+  const daysOfWeek = [ 
+    "Воскресенье", 
+    "Понедельник", 
+    "Вторник", 
+    "Среда", 
+    "Четверг", 
+    "Пятница", 
+    "Суббота" 
+  ]; 
+ 
+  const months = [ 
+    "Январь", 
+    "Февраль", 
+    "Март", 
+    "Апрель", 
+    "Май", 
+    "Июнь", 
+    "Июль", 
+    "Август", 
+    "Сентябрь", 
+    "Октябрь", 
+    "Ноябрь", 
+    "Декабрь" 
+  ]; 
+ 
+  const dayOfWeek = daysOfWeek[time.getDay()]; 
+  const month = months[time.getMonth()]; 
+  const day = time.getDate(); 
+  const year = time.getFullYear(); 
+  const hours = time.getHours().toString().length < 2 ? '0' + time.getHours() : time.getHours();
+  const minutes = time.getMinutes().toString().length < 2 ? '0' + time.getMinutes() : time.getMinutes();
+  const seconds = time.getSeconds(); 
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  
+
+
+  
+
+  return (  
+ <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-gray-800 shadow-sm rounded-xl">
       <div className="px-5 pt-5">
         <header className="flex justify-between items-start mb-2">
-          <h2 className="text-lg font-semibold text-gray-800 dark:text-gray-100 mb-2">Acme Plus</h2>
+          <h2 className="text-lg font-semibold text-gray-800 dark:text-gray-100 mb-2">Время</h2>
           {/* Menu button */}
           <EditMenu align="right" className="relative inline-flex">
             <li>
@@ -103,19 +98,23 @@ function DashboardCard01() {
             </li>
           </EditMenu>
         </header>
-        <div className="text-xs font-semibold text-gray-400 dark:text-gray-500 uppercase mb-1">Sales</div>
+        <div className="text-xs font-semibold text-gray-400 dark:text-gray-500 uppercase mb-1">{dayOfWeek} {day} {month}</div>
         <div className="flex items-start">
-          <div className="text-3xl font-bold text-gray-800 dark:text-gray-100 mr-2">$24,780</div>
-          <div className="text-sm font-medium text-green-700 px-1.5 bg-green-500/20 rounded-full">+49%</div>
+          <div className="text-3xl font-bold text-gray-800 dark:text-gray-100 mr-2">{hours}:{minutes}</div>
+          <div className="text-sm font-medium text-red-700 px-1.5 bg-red-500/20 rounded-full text-align: center">Закат в {getRise('sunset')}</div>
+          <div className="text-sm font-medium text-green-700 px-1.5 bg-green-500/20 rounded-full">Восход в {getRise('sunrise')}</div>
         </div>
       </div>
       {/* Chart built with Chart.js 3 */}
-      <div className="grow max-sm:max-h-[128px] xl:max-h-[128px]">
+      <div className="grow max-sm:max-h-[128px] max-h-[128px]">
         {/* Change the height attribute to adjust the chart height */}
-        <LineChart data={chartData} width={389} height={128} />
+        проверка
       </div>
     </div>
-  );
+  
+  ); 
 }
+
+
 
 export default DashboardCard01;
