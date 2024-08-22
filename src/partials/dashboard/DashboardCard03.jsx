19160e49a57f809d4d6994e5895226c9070d4da2
Sweet Jesus <bryloveeugene@gmail.com>
2024-08-22 15:45:29 +0500

    last1

--- src/partials/dashboard/DashboardCard03.jsx
+++ src/partials/dashboard/DashboardCard03.jsx
@@ -1,32 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import LineChart from '../../charts/LineChart01';
-import { chartAreaGradient } from '../../charts/ChartjsConfig';
+
 import EditMenu from '../../components/DropdownEditMenu';
 import { YMaps, Map, TrafficControl, FullscreenControl } from '@pbe/react-yandex-maps';
 
 // Import utilities
-import { tailwindConfig, hexToRGB } from '../../utils/Utils';
 
-function getHoro(a) {
-  const [userData, setUserData] = useState([]);
-  // fetching api data
-  useEffect(() => {
-    
-    fetch(`http://anecdotica.ru/api`,{
-method: 'GET',
-token : '••••••••••••••••••••'
-    })
-      .then(res => res.json())
-      .then(data => setUserData(data))
-    
-  }, []
-);
-console.log(userData)
-return (
-<div></div>
-)
-}
+
 
 function DashboardCard03() {
 
@@ -35,23 +15,23 @@ function DashboardCard03() {
     <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-gray-800 shadow-sm rounded-xl">
       <div className="px-5 pt-5">
         <header className="flex justify-between items-start mb-2">
-          <h2 className="text-lg font-semibold text-gray-800 dark:text-gray-100 mb-2">Гороскоп на сегодня</h2>
+          <h2 className="text-lg font-semibold text-gray-800 dark:text-gray-100 mb-2">Пермский край, Пермь</h2>
           {/* Menu button */}
           <EditMenu align="right" className="relative inline-flex">
             <li>
               <Link className="font-medium text-sm text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-200 flex py-1 px-3" to="#0">
-                Дева
+                опция 1
               </Link>
             </li>
             <li>
               <Link className="font-medium text-sm text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-200 flex py-1 px-3" to="#0">
-                Водолей
+                опция 2
               </Link>
             </li>
             
           </EditMenu>
         </header>
-        <div className="text-xs font-semibold text-gray-400 dark:text-gray-500 uppercase mb-1">Водолей</div>
+        <div className="text-xs font-semibold text-gray-400 dark:text-gray-500 uppercase mb-1">Яндекс.Карты</div>
         <div className="flex items-start">
           <div className="text-3xl font-bold text-gray-800 dark:text-gray-100 mr-2">
           
