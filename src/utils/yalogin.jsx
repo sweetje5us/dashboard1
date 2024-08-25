4f2c508e374d0faf84c0333d23850343569b685c
Sweet Jesus <bryloveeugene@gmail.com>
2024-08-25 15:58:33 +0500

    dsad

--- src/utils/yalogin.jsx
+++ src/utils/yalogin.jsx
@@ -1,5 +1,5 @@
 
-export default function yaWidget(){
+function yaWidget2(){
     YaAuthSuggest.init(
   {
     client_id: 'fb308504b0844f1eb57c405e1de5ca63',
@@ -21,17 +21,27 @@ export default function yaWidget(){
   }) => handler())
   .then(data => console.log('Сообщение с токеном', data))
   .catch(error => console.log('Обработка ошибки', error));
-  window.YaAuthSuggest.init(
-    {
-      client_id: "fb308504b0844f1eb57c405e1de5ca63",
-      response_type: "token",
-      redirect_uri: "https://dashboard1-tau.vercel.app/"
-    },
-    "https://dashboard1-tau.vercel.app/#",
-    { view: "default" }
-  )
-  .then(({handler}) => handler())
-  .then(data => console.log('Сообщение с токеном', data))
-  .catch(error => console.log('Обработка ошибки', error)) 
 }
 
+
+
+export default function yaWidget() {
+    window.YaAuthSuggest.init(
+        {
+          client_id: "fb308504b0844f1eb57c405e1de5ca63",
+          response_type: "token",
+          redirect_uri: "https://dashboard1-tau.vercel.app/"
+        },
+        "https://dashboard1-tau.vercel.app/#",
+        { view: "default" }
+      )
+      .then(({handler}) => handler())
+      .then(data => console.log('Сообщение с токеном', data))
+      .catch(error => console.log('Обработка ошибки', error))
+   
+    return (
+        <div>
+        {yaWidget2()}
+        </div>         
+    );
+  }
