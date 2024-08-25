b538324c246dfa08660e821f64f1e47952b805ed
Sweet Jesus <bryloveeugene@gmail.com>
2024-08-25 19:51:27 +0500

    fdf

--- src/utils/yalogin.jsx
+++ src/utils/yalogin.jsx
@@ -5,9 +5,9 @@ function yaWidget(){
   {
     client_id: 'b91f0d61be1f498f9c607cfa5c0c9164',
     response_type: 'token',
-    redirect_uri: 'https://dashboard1-tau.vercel.app/'
+    redirect_uri: 'https://dashboard1-hmnl.vercel.app/'
   },
-  'https://dashboard1-tau.vercel.app', 
+  'https://dashboard1-hmnl.vercel.app/', 
   {
     view: 'button',
     parentId: 'container',
